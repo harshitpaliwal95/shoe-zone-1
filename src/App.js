220a86809b8328eb14502f897b714a654952320d
Harshit Paliwal <harshitpaliwal95@gmail.com>
2022-03-20 20:43:28 +0530

    Feat: add Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
-import { home as Home } from "./pages/home/home";
+
+import { Home, Login, Join, Wishlist, Cart, Product } from "./pages/index";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/join" element={<Join />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/product" element={<Product />} />
+      </Routes>
     </div>
   );
 }
