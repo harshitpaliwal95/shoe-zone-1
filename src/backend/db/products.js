66f634dc74cead860c3456d32a9ecca52b7cf9c2
Harshit Paliwal <harshitpaliwal95@gmail.com>
2022-03-21 21:20:35 +0530

    Add: data to database

--- src/backend/db/products.js
+++ src/backend/db/products.js
@@ -1,30 +1,70 @@
 import { v4 as uuid } from "uuid";
-
-/**
- * Product Database can be added here.
- * You can add products of your wish with different attributes
- * */
-
+// man catagory images
+import nike_air_max_270 from "./db-assets/nike-air-max-270.webp";
+import nike_air_zoom_AIR from "./db-assets/air-zoom-pegasus-38-air-jordan-moss-mens-road-running-shoes-lq7PZZ.jpg";
+import nike_air_max_90 from "./db-assets/Nike Air Max 90.webp";
+import nike_blazer_mid_77 from "./db-assets/nike blazer mid77.webp";
+import Nike_air_force_107 from "./db-assets/nike air force 107.webp";
+import nike_react_miler from "./db-assets/nike react miler 2 shield.webp";
+import nike_bassi_jdi from "./db-assets/nike benasi jdi.webp";
+import nike_air_max_terrace_cape90 from "./db-assets/nike air max terrascape 90.webp";
+// women catagory
+// import nike_blazer_low from "./db-assets/nike blazer low platform next nature.webp";
 export const products = [
   {
     _id: uuid(),
-    title: "You Can WIN",
-    author: "Shiv Khera",
-    price: "5000",
-    categoryName: "non-fiction",
+    name: "Nike air max 270",
+    price: 8999,
+    image: { nike_air_max_270 },
+    categoryName: "mens",
   },
   {
     _id: uuid(),
-    title: "You are Winner",
-    author: "Junaid Qureshi",
-    price: "3000",
-    categoryName: "horror",
+    name: "Nike Air Zoom Pegasus 38 A.I.R.",
+    price: 9100,
+    image: { nike_air_zoom_AIR },
+    categoryName: "mens",
   },
   {
     _id: uuid(),
-    title: "Think and Grow Rich",
-    author: "Shiv Khera",
-    price: "1000",
-    categoryName: "fiction",
+    name: "Nike Air Max 90",
+    price: 7000,
+    image: { nike_air_max_90 },
+    categoryName: "mens",
+  },
+  {
+    _id: uuid(),
+    name: "Nike Blazer Mid '77",
+    price: 6500,
+    image: { nike_blazer_mid_77 },
+    categoryName: "mens",
+  },
+  {
+    _id: uuid(),
+    name: "nike air  force 1 07",
+    price: 8000,
+    image: { Nike_air_force_107 },
+    categoryName: "mens",
+  },
+  {
+    _id: uuid(),
+    name: "nike react miler 2 shiield",
+    price: 7599,
+    image: { nike_react_miler },
+    categoryName: "mens",
+  },
+  {
+    _id: uuid(),
+    name: "Nike Bassi JDI",
+    price: 1000,
+    image: { nike_bassi_jdi },
+    categoryName: "mens",
+  },
+  {
+    _id: uuid(),
+    name: "Nike Air Max Terrace 90",
+    price: 9999,
+    image: { nike_air_max_terrace_cape90 },
+    categoryName: "mens",
   },
 ];
