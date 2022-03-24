b6e43bf96b241d3b637441ba6f78d566faa0ab8f
Harshit Paliwal <harshitpaliwal95@gmail.com>
2022-03-24 22:38:13 +0530

    Add: higher order function in reduce

--- src/reducer/filterReducer.js
+++ src/reducer/filterReducer.js
@@ -1,24 +1,33 @@
-import { ACTION } from "../action/action";
 export const filterReducer = (state, action) => {
+  const clearState = {
+    product: [],
+
+    sortBy: null,
+    category: {
+      men: false,
+      women: false,
+      kid: false,
+      latest: false,
+    },
+  };
+
+  const { men, women, kid, latest } = state;
   const { type, payload } = action;
   switch (type) {
-    case ACTION.GET_DATA:
-      return { ...state, product: payload };
-    case ACTION.HIGH_TO_LOW:
-      return {
-        ...state,
-        shortBy: state.product.sort((a, b) => b.price - a.price),
-      };
-    case ACTION.LOW_TO_HIGH:
-      return {
-        ...state,
-        shortBy: state.product.sort((a, b) => a.price - b.price),
-      };
-    case ACTION.CATEGORY:
-      return {
-        ...state,
-        category: state.product.filter((item) => item.categoryName === payload),
-      };
+    case "sortBy":
+      return { ...state, sortBy: action.payload };
+    case "Men":
+      return { ...state, category: { ...state.category, men: !men } };
+    case "Women":
+      return { ...state, category: { ...state.category, women: !women } };
+    case "Kid":
+      return { ...state, category: { ...state.category, kid: !kid } };
+    case "latest":
+      return { ...state, category: { ...state.category, latest: !latest } };
+    case "rating":
+      return { ...state, rating: parseInt(action.payload) };
+    case "clear":
+      return clearState;
     default:
       return state;
   }
