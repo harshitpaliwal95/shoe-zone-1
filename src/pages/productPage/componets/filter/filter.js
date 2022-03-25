1e71ba94b8c523740ae16be8534e1fbc789150ec
Harshit Paliwal <harshitpaliwal95@gmail.com>
2022-03-25 15:51:50 +0530

    Fix: remove btn

--- src/pages/productPage/componets/filter/filter.js
+++ src/pages/productPage/componets/filter/filter.js
@@ -114,15 +114,6 @@ export const Filter = () => {
             }}
           />
         </div>
-        <button
-          className="btn btn-dark filter-btn"
-          onClick={() => {
-            console.log("clicked");
-            dispatch({ type: "clear" });
-          }}
-        >
-          Clear
-        </button>
       </div>
     </aside>
   );
