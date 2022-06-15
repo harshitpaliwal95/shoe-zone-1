ad8a63f325bcb31cdfa59ead5470eefb5ddeca73
Harshit Paliwal <harshitpaliwal95@gmail.com>
2022-06-15 17:33:32 +0530

    FEAT: added protected routing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Home, Login, Wishlist, Cart, Product, SignUp } from "./pages/index";
 import { Routes, Route } from "react-router-dom";
 import Mockman from "mockman-js";
 import { ToastContainer } from "react-toastify";
+import { ProtectedRoute } from "./hook/protectedRoute";
 
 function App() {
   return (
@@ -13,9 +14,30 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/wishlist" element={<Wishlist />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/product" element={<Product />} />
+        <Route
+          path="/wishlist"
+          element={
+            <ProtectedRoute>
+              <Wishlist />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/cart"
+          element={
+            <ProtectedRoute>
+              <Cart />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/product"
+          element={
+            <ProtectedRoute>
+              <Product />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/mockman" element={<Mockman />} />
       </Routes>
     </div>
