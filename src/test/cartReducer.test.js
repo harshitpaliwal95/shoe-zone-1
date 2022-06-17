a9d0627a69f5f92ba9d9b06809ad7b7a577487d4
Harshit Paliwal <harshitpaliwal95@gmail.com>
2022-06-17 22:24:43 +0530

    TEST: dic item

--- src/test/cartReducer.test.js
+++ src/test/cartReducer.test.js
@@ -177,4 +177,74 @@ describe("testing cart reducer", () => {
       ],
     });
   });
+  test("should dicrease quantity", () => {
+    const initial = {
+      cartItem: [
+        {
+          _id: 12345,
+          productName: "Nike Air Zoom 38 A.I.R.",
+          price: 9100,
+          image: "nike_air_zoom_AIR",
+          categoryName: "man",
+          discount: 20,
+          latest: false,
+          rating: 4,
+          quantity: 3,
+        },
+        {
+          _id: 1234,
+          productName: "Nike Air Max 270",
+          price: 8999,
+          image: "nike_air_max_270.url",
+          categoryName: "man",
+          discount: 30,
+          latest: true,
+          rating: 3,
+          quantity: 3,
+        },
+      ],
+    };
+    const action = {
+      type: "DIC_ITEM",
+      payload: {
+        _id: 1234,
+        productName: "Nike Air Max 270",
+        price: 8999,
+        image: "nike_air_max_270.url",
+        categoryName: "man",
+        discount: 30,
+        latest: true,
+        rating: 3,
+        quantity: 3,
+      },
+    };
+    const state = cartReducer(initial, action);
+
+    expect(state).toEqual({
+      cartItem: [
+        {
+          _id: 12345,
+          productName: "Nike Air Zoom 38 A.I.R.",
+          price: 9100,
+          image: "nike_air_zoom_AIR",
+          categoryName: "man",
+          discount: 20,
+          latest: false,
+          rating: 4,
+          quantity: 3,
+        },
+        {
+          _id: 1234,
+          productName: "Nike Air Max 270",
+          price: 8999,
+          image: "nike_air_max_270.url",
+          categoryName: "man",
+          discount: 30,
+          latest: true,
+          rating: 3,
+          quantity: 2,
+        },
+      ],
+    });
+  });
 });
